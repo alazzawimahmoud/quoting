e8a4ced91e6aa23ed6765ab2ee1d6c82d49ae826
mahmoud.alazzawi <mahmoud.alazzawi@seaters.com>
2017-10-05 16:54:18 +0100

    working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject} from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http'
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  url = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1';
+  qoute$;
+  modes = [
+    {name : 'fancy-mode'},
+    {name : 'normal-mode'},
+    {name : 'dark-mode'}
+  ];
+  activeMode : any = {name:'fancy-mode'};
+  constructor (private http: Http) {}
+  switchMode(selectedMode) {
+    document.body.classList.remove(this.activeMode.name)    
+    this.activeMode = selectedMode;
+    document.body.classList.toggle(this.activeMode.name)    
+  }
+  ngOnInit(){
+    this.switchMode(this.activeMode);
+    let headers = new Headers({ 'X-Mashape-Key': 'BZ2aO1hLwhmshyVeJxLqoa9pev9Cp1XsDCQjsnWApMQfHvJ0Pv', 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    let getQoute$ = this.http.get(this.url, options);
+    let initial$ = Observable.from(getQoute$);
+    let fromInterval$ = Observable.interval(15000).switchMap(() => getQoute$);
+    return this.qoute$ = Observable.merge(initial$, fromInterval$).map(d => d.json())
+  }
+  
 }

de1313a118e4a20ec2618799765ad440726d4814
Mahmoud <mahmoud@Azzawis-MacBook-Pro.local>
2018-09-22 14:15:31 +0200

    Minor fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject} from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http'
+import { Http, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
 @Component({
@@ -15,21 +15,21 @@ export class AppComponent implements OnInit {
     {name : 'normal-mode'},
     {name : 'dark-mode'}
   ];
-  activeMode : any = {name:'fancy-mode'};
+  activeMode : any = {name: 'fancy-mode'};
   constructor (private http: Http) {}
   switchMode(selectedMode) {
-    document.body.classList.remove(this.activeMode.name)    
+    document.body.classList.remove(this.activeMode.name);
     this.activeMode = selectedMode;
-    document.body.classList.toggle(this.activeMode.name)    
+    document.body.classList.toggle(this.activeMode.name);
   }
   ngOnInit(){
     this.switchMode(this.activeMode);
-    let headers = new Headers({ 'X-Mashape-Key': 'BZ2aO1hLwhmshyVeJxLqoa9pev9Cp1XsDCQjsnWApMQfHvJ0Pv', 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-    let getQoute$ = this.http.get(this.url, options);
-    let initial$ = Observable.from(getQoute$);
-    let fromInterval$ = Observable.interval(15000).switchMap(() => getQoute$);
-    return this.qoute$ = Observable.merge(initial$, fromInterval$).map(d => d.json())
+    const headers = new Headers({ 'X-Mashape-Key': 'BZ2aO1hLwhmshyVeJxLqoa9pev9Cp1XsDCQjsnWApMQfHvJ0Pv', 'Content-Type': 'application/json' });
+    const options = new RequestOptions({ headers: headers });
+    const getQoute$ = this.http.get(this.url, options);
+    const initial$ = Observable.from(getQoute$);
+    const fromInterval$ = Observable.interval(15000).switchMap(() => getQoute$);
+    return this.qoute$ = Observable.merge(initial$, fromInterval$).map(d => d.json()[0]);
   }
-  
+
 }
